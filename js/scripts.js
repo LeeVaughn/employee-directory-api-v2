c39ec2daf28ab57f5829e7e0a045293cc3787ebb
LeeVaughn <dlvprogramming@gmail.com>
2019-01-29 15:58:53 -0800

    employee cards being displayed

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,20 @@ const url = "https://randomuser.me/api/?results=12&nat=us&exc=gender,registered,
 // retreives data from the random user API
 $.getJSON(url, (data) => {
   console.log(data.results);
+  for (let i = 0; i < data.results.length; i++) {
+    const $userHTML = $(`
+      <div class="card">
+        <div class="card-img-container">
+          <img class="card-img" src="${data.results[i].picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+          <p id="username" class="d-none">${data.results[i].login.username}</p>
+          <p class="card-text">${data.results[i].email}</p>
+          <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+        </div>
+      </div>
+    `);
+    $("#gallery").append($userHTML);
+  }
 })
